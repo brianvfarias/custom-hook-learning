836092965c00ae4eb80fac337cf47228e89d5a9a
Brian Farias <brianvicthorandp@gmail.com>
2024-01-17 13:27:22 -0300

    Refactoring to practice useReducer! :nerd_face:

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { useCount } from "./hooks/useCount";
 export function App() {
-  const { count, increase, decrease, reset } = useCount(1, 2);
+  const { countData, dispatch } = useCount({ initial: 1 });
 
   return (
     <div>
       <p>Unit to purchase</p>
-      <button onClick={(e: React.MouseEvent<HTMLElement>) => reset(e)}>Default</button>
-      <button onClick={(e: React.MouseEvent<HTMLElement>) => decrease(e)}>-</button>
-      <input type="number" value={count.toString()} readOnly />
-      <button onClick={(e: React.MouseEvent<HTMLElement>) => increase(e)}>+</button>
       <button onClick={(e: React.MouseEvent<HTMLElement>) => {
-        alert(`You selected ${count} many units to purchase!`)
-        reset(e)
+        e.preventDefault();
+        dispatch({ type: 'RESET' });
+      }}>Default</button>
+      <button onClick={(e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        dispatch({ type: 'DECREMENT' });
+      }}>-</button>
+      <input type="number" value={countData.count.toString()} readOnly />
+      <button onClick={(e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        dispatch({ type: 'INCREMENT' });
+      }}>+</button>
+      <button onClick={(e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        alert(`You selected ${countData.count} many units to purchase!`)
+        dispatch({ type: 'RESET' })
       }}>Purchase</button>
 
     </div >
--- src/hooks/useCount.ts
+++ src/hooks/useCount.ts
@@ -1,31 +1,83 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 /*
 A cool feature to implement would be a counterConfig 
 
 basically a optional argument for the Use Count Hook, that would define the highest and lowest limits of the counter etc.
 */
+interface UseCountParams {
+  initial: number
+  step?: number
+  allowNegative?: boolean
+}
 
-export function useCount(initial = 0, step = 1, allowNegative = false) {
-  if (step <= 0) {
-    step = 1
-  }
-  const [count, setCount] = useState(initial)
-  function increase(e: React.MouseEvent<HTMLElement>) {
-    e.preventDefault()
-    return setCount((prev) => prev + step)
-  }
+interface CountState extends UseCountParams {
+  count: number
+}
 
-  function decrease(e: React.MouseEvent<HTMLElement>) {
-    e.preventDefault()
-    if (allowNegative) {
-      return setCount((prev) => prev - step)
+interface ReducerAction {
+  type: string
+}
+
+function reducer(state: CountState, action: ReducerAction) {
+  // Workaround to some TS errors
+  const { step } = state
+  const currentStep = step ? step : 1
+  // const currentCount = count ? count : 0
+
+  switch (action.type) {
+    case 'INCREMENT': {
+      return { ...state, count: state.count + currentStep }
     }
-    return setCount(count - step >= 0 ? count - step : 0)
+    case 'DECREMENT': {
+      if (state.allowNegative) {
+        return { ...state, count: state.count - currentStep }
+      }
+      return {
+        ...state,
+        count: state.count - currentStep >= 0 ? state.count - currentStep : 0,
+      }
+    }
+    case 'RESET': {
+      return { ...state, count: state.initial }
+    }
+    default: {
+      return state
+    }
   }
+}
 
-  function reset(e: React.MouseEvent<HTMLElement>) {
-    e.preventDefault()
-    return setCount(initial)
+export function useCount({
+  initial = 0,
+  step = 1,
+  allowNegative = false,
+}: UseCountParams) {
+  const countInfo = {
+    count: initial,
+    initial,
+    step: step ? 1 : step,
+    allowNegative,
   }
-  return { count, increase, decrease, reset }
+  const [countData, dispatch] = useReducer(reducer, countInfo)
+  // if (step <= 0) {
+  //   step = 1
+  // }
+  // const [count, setCount] = useState(initial)
+  // function increase(e: React.MouseEvent<HTMLElement>) {
+  //   e.preventDefault()
+  //   return setCount((prev) => prev + step)
+  // }
+
+  // function decrease(e: React.MouseEvent<HTMLElement>) {
+  //   e.preventDefault()
+  //   if (allowNegative) {
+  //     return setCount((prev) => prev - step)
+  //   }
+  //   return setCount(count - step >= 0 ? count - step : 0)
+  // }
+
+  // function reset(e: React.MouseEvent<HTMLElement>) {
+  //   e.preventDefault()
+  //   return setCount(initial)
+  // }
+  return { countData, dispatch }
 }
